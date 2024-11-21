e50d4e4a76b3a335bb15cc60cbefeb814494c0a0
André Kovac <me@andrekovac.com>
2024-11-21 20:24:53 +0100

    18 - Sentry: Integrate Sentry for error tracking and monitoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, StyleSheet } from "react-native";
+import { Button, SafeAreaView, StyleSheet } from "react-native";
+import * as Sentry from "@sentry/react-native";
 
 import "../global.css";
 
 import MainScreen from "./screens/MainScreen";
 import Header from "./components/Header";
+
+Sentry.init({
+  dsn: "https://51ab27600424f934d3311b01a3ff458a@o376817.ingest.us.sentry.io/4508337761157120",
+});
 
 const App = () => {
   return (
@@ -12,6 +17,12 @@ const App = () => {
       <Header />
       <StatusBar style="auto" />
       <MainScreen />
+      <Button
+        title="Test Sentry Error!"
+        onPress={() => {
+          throw new Error("Intentional error");
+        }}
+      />
     </SafeAreaView>
   );
 };
